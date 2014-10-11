931298b68783e06920b62aa32fd07f5ee31d8aff
itaymendl <itay.mendel@blazemeter.com>
2014-10-11 12:40:34 -0700

    add mok jmx

--- script/loadosophia.sh
+++ script/loadosophia.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # apt-get install openjdk-6-jre
+
+mkdir jmeter
+wget -O jmeter/test.jmx https://s3.amazonaws.com/blazemeter/install/itay/test.jmx
 wget -O jmeter/apache-jmeter-2.11.tgz http://apache.spd.co.il//jmeter/binaries/apache-jmeter-2.11.tgz
 tar zxf jmeter/apache-jmeter-2.11.tgz -C jmeter/
-
 jmeter/apache-jmeter-2.11/bin/jmeter -n -t jmeter/test.jmx -l jmeter/results.jtl
 gzip jmeter/results.jtl
 
