da3db5593450e6dca26fe0e1604807334dfa0476
itaymendl <itay.mendelawy@gmail.com>
2014-12-07 16:59:27 +0200

    fix

--- script/loadosophia.sh
+++ script/loadosophia.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir jmeter
-wget -O jmeter/apache-jmeter-2.11.tgz http://apache.spd.co.il//jmeter/binaries/apache-jmeter-2.11.tgz
-tar zxf jmeter/apache-jmeter-2.11.tgz -C jmeter/
-jmeter/apache-jmeter-2.11/bin/jmeter -n -t test/backend.jmx -l jmeter/results.jtl -Jhost=localhost
+wget -O jmeter/apache-jmeter-2.12.tgz http://apache.spd.co.il//jmeter/binaries/apache-jmeter-2.12.tgz -q
+tar zxf jmeter/apache-jmeter-2.12.tgz -C jmeter/
+jmeter/apache-jmeter-2.12/bin/jmeter -n -t test/backend.jmx -l jmeter/results.jtl -Jhost=localhost
 gzip jmeter/results.jtl
 
 curl -v https://loadosophia.org/api/file/upload/ -H "X-Loadosophia-Token: `cat .loadosophia.token`" -F "projectKey=DEFAULT" -F "jtl_file=@jmeter/results.jtl.gz"
