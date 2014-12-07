758da80d6191ecb0b903ed82f417682ed3653498
itaymendl <itay.mendelawy@gmail.com>
2014-12-07 17:12:01 +0200

    da

--- script/loadosophia.sh
+++ script/loadosophia.sh
@@ -3,7 +3,6 @@
 mkdir jmeter
 wget -O jmeter/apache-jmeter-2.12.tgz http://apache.spd.co.il//jmeter/binaries/apache-jmeter-2.12.tgz -q
 tar zxf jmeter/apache-jmeter-2.12.tgz -C jmeter/
-jmeter/apache-jmeter-2.12/bin/jmeter -n -t test/backend.jmx -l jmeter/results.jtl -Jhost=localhost
-gzip jmeter/results.jtl
-
-curl -v https://loadosophia.org/api/file/upload/ -H "X-Loadosophia-Token: `cat .loadosophia.token`" -F "projectKey=DEFAULT" -F "jtl_file=@jmeter/results.jtl.gz"
+jmeter/apache-jmeter-2.12/bin/jmeter -n -t test/backend.jmx -l results.jtl -Jhost=localhost
+gzip results.jtl
+curl https://loadosophia.org/api/file/upload/ -H "X-Loadosophia-Token: `cat .loadosophia.token`" -F "projectKey=DEFAULT" -F "jtl_file=@results.jtl.gz"
